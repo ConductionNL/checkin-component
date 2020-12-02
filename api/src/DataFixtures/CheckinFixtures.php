10ef11ace7c7e016d4619c10ca065f957db184e4
Robert Zondervan <Robert@conduction.nl>
2020-12-02 16:16:52 +0100

    Value updates

--- api/src/DataFixtures/CheckinFixtures.php
+++ api/src/DataFixtures/CheckinFixtures.php
@@ -5,7 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Node;
 use Conduction\CommonGroundBundle\Service\CommonGroundService;
 use Doctrine\Bundle\FixturesBundle\Fixture;
-use Doctrine\Common\Persistence\ObjectManager;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 
 class CheckinFixtures extends Fixture
